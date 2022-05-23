938c7a9e474376a55b41f447a3e21d6bb01b9ce0
Ismail Musa <106095327+elmiszput1@users.noreply.github.com>
2022-05-23 15:54:30 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <!--Matarial icon-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/fantasyStyl.css">
+    <title>Fantasy</title>
+</head>
+
+<body class="fantasy_body">
+    <div class="container-fluid bg-light">
+        <div class="row">
+            <section class="fantasy_sectionTop">
+
+                <div class="col">
+                    <div class="fantasy_card fantasy_One">
+                        <h3 class="fantasy_prnwin">PREDICT AND WIN</h3>
+                        <h3 class="fantasy_hundred">$100</h3>
+                        <h3 class="fantasy_participt">Participate and stand a chance to win every week</h3>
+                        <div class="fantasy_sectionMiddBTN">
+                            <div class="fantasy_pff"> Play For Free
+                                <span class="fantasy_material-icons-sharp"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="fantasy_card fantasy_two">
+                        <div class="fantasy_twoCard">
+                            <img src="img/betensuredLogoCrop1.png">
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+
+
+
+
+    <section class="fantasy_sectionMiddle">
+        <div class="fantasy_sectionMiddleInner">
+            <div class="fantasy_sectionMiddleCard">
+                <div class="fantasy_secOneInner">
+                    <h1>1</h1>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <h4> Play Our Lucky 5 Selections For FREE</h4>
+                </div>
+            </div>
+            <div class="fantasy_sectionMiddleCard">
+                <div class="fantasy_secOneInner">
+                    <h1>2</h1>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <h4> Predict All 5 Correct Outcome to Win The $100 Jackpot</h4>
+                </div>
+            </div>
+            <div class="fantasy_sectionMiddleCard">
+                <div class="fantasy_secOneInner">
+                    <h1>3</h1>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <h4> $20 is Guaranteed To Be Won Every Week, If No One Wins The Jackpot</h4>
+                </div>
+            </div>
+            <div class="fantasy_sectionMiddleCard" style="border:2px solid #B96C07;">
+                <div class="fantasy_secOneInner">
+                    <h1>4</h1>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <h4> Only <span class="beplus"><img src="img/Betplus.jpg" alt="" width="120px" height="20px"></span> Members are Eligible to Win $100 Jackpot. Non-Members Are Only Eligible to Win $20
+                    </h4>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--<section class="sectionBottom">
+		<div class="dmcation">
+			<span class="plogo"><img src="img/mc_acc_v2.svg"></span>
+			<span class="plogo"><img src="img/visa_white_v2.svg"></span>
+			<span class="plogo"><img src="img/verve_v2.svg"></span>
+			<span class="plogo"><img src="img/interswitch.svg"></span>
+	</div>
+	<div class="dmcation">
+
+<span class="plogo"><img src="img/Paystack_v2.svg"></span>
+<span class="plogo"><img src="img/verve_v2.svg"></span>
+</div>
+</section>-->
+
+
+    <section class="fantasy_sectionBottom">
+        <div class="fantasy_dmcation">
+            <ul>
+                <li><a href="">Privacy Polic</a>y</li>
+                <li><a href="">Terms and Condition</a></li>
+                <li><a href="#">Game Rules</a></li>
+                <li><a href="">Cookie Polic</a>y</li>
+            </ul>
+        </div>
+
+    </section>
+
+
+
+    <div class="fantasy_mviewContainer">
+        <section class="fantasy_mobile-button fantasy_enter-view">
+            <a href="fantasy.php">
+                <span class="fantasy_button">Play Now</span>
+            </a>
+        </section>
+    </div>
+
+
+
+</body>
+
+</html>
